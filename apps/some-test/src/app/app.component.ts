dcbfe8a7072abd1d18718c96ffc6f878574896d8
Kirill Metrik <kirillmetrik@gmail.com>
2022-01-03 18:23:32 +0100

    initial commit

---
+++ apps/some-test/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'story-test-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less'],
+})
+export class AppComponent {
+  title = 'some-test';
+}
